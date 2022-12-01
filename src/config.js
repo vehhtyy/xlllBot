7f69ff11588a9125688a33828fde139fda12580f
vehhty <84939585+vehhtyy@users.noreply.github.com>
2022-12-01 19:08:04 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 const apiEndPoint = 'http://localhost:1337'
-const botUsername = 'xlllbot' // your bot username on twitch
+const botUsername = 'vehhtyBot' // your bot username on twitch
 
 export { apiEndPoint, botUsername }
